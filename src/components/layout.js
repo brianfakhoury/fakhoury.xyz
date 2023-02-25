5b77c6791110d4ac51303834fcc63159fc12505b
Brian Fakhoury <brianfakhoury@gmail.com>
2023-02-25 12:57:58 -0500

    og type

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,6 +31,8 @@ export default function Layout({ children }) {
           content="brian fakhoury, venture capital, machine learning, neuroscience, crypto, blockchain, defi, lifestyle, personal page"
         />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@brianfakhoury" />
+        <meta name="twitter:creator" content="@brianfakhoury" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:type" content="image/png" />
