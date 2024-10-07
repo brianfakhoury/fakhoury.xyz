d0d68c4dc6207d9a6d1387e9fed5fba736344518
Brian Fakhoury <brianfakhoury@gmail.com>
2024-10-07 22:37:21 +0200

    new og architecture again

--- app/[slug]/og.png/route.tsx
+++ app/[slug]/og.png/route.tsx
@@ -1,6 +1,8 @@
 import getPosts, { getPost } from "@/lib/get-posts";
 import { ImageResponse } from "next/og";
 import { NextResponse, type NextRequest } from "next/server";
+import fs from "fs";
+import path from "path";
 
 export const dynamicParams = false;
 
@@ -13,6 +15,19 @@ export async function generateStaticParams() {
   return posts.map((post) => ({ slug: post.slug }));
 }
 
+function getBase64Image(filePath: string): string {
+  const imagePath = path.resolve(process.cwd(), filePath);
+  const imageBuffer = fs.readFileSync(imagePath);
+
+  // Determine the file extension to set the correct MIME type
+  const ext = path.extname(filePath).toLowerCase();
+  const mimeType =
+    ext === ".jpg" || ext === ".jpeg" ? "image/jpeg" : "image/png";
+
+  // Return the image as a base64-encoded string
+  return `data:${mimeType};base64,${imageBuffer.toString("base64")}`;
+}
+
 export async function GET(req: NextRequest, { params }: RouteProps) {
   const size = { width: 1200, height: 630 };
 
@@ -20,12 +35,14 @@ export async function GET(req: NextRequest, { params }: RouteProps) {
 
   if (!post) return new NextResponse("Not found", { status: 404 });
 
-  const host = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : `http://localhost:${process.env.PORT || 3000}`;
+  const cover_path = post.image
+    ? path.join("content", "posts", post.image)
+    : "";
+  const logo_path = path.join("app", "icon.png");
 
-  const cover_src = post.image ? `${host}/${post.image}` : "";
-  const logo_src = `${host}/icon.png`;
+  const cover_src = cover_path ? getBase64Image(cover_path) : "";
+  const logo_src = getBase64Image(logo_path);
+
   const text_size = post.title.length > 20 ? "text-8xl" : "text-9xl";
 
   return new ImageResponse(
